fce190cfb40aefcc302450d790bab5329abd8e76
alsplatonov <platon1560@gmail.com>
2022-10-06 00:46:09 +0300

    review fixes

--- 09-is-prime/script.js
+++ 09-is-prime/script.js
@@ -13,7 +13,7 @@ function isPrime(n) {
     if (n === 1 || n === 0) {
         return false;
     } else {
-        for (let i = 2; i < n; i++) {
+        for (let i = 2; i < n * 0.5 + 1; i++) {
             if (n % i === 0) {
                 return false;
             }
